3896053db46e4d67c457c809289d161f5609a1b3
Pedro Pires <ppires000@gmail.com>
2014-11-12 00:46:46 -0200

    airports and programs

---
+++ programs.php
@@ -0,0 +1,86 @@
+<?php
+
+$programs = array(
+     "tam_miles" => array(
+                    // these fields MUST be edited.
+                               "price" => "310.00",
+                           "available" => "1000000",
+                        "selling_mode" => "1",
+             "maximizing_profit_start" => "0.00",
+        "maximizing_profit_percentage" => "0.00",
+
+                    // leave these as they are.
+                                  "id" => "1",
+                                "hash" => "tam_miles",
+                             "acronym" => "TAM",
+                             "airline" => "TAM",
+                                "name" => "Milhas Tam",
+                           "kind_name" => "milhas",
+                    "miles_enterprise" => "Multiplus",
+                      "activated_sell" => "1",
+                               "color" => "#BA1E1E",
+                              "wsname" => "tam"
+    ),
+    "azul_miles" => array(
+                    // these fields MUST be edited.
+                               "price" => "371.00",
+                           "available" => "1000000",
+                        "selling_mode" => "1",
+             "maximizing_profit_start" => "0.00",
+        "maximizing_profit_percentage" => "0.00",
+
+                    // leave these as they are.
+                                  "id" => "2",
+                                "hash" => "azul_miles",
+                             "acronym" => "AZUL",
+                             "airline" => "AZUL",
+                                "name" => "Milhas Azul",
+                           "kind_name" => "milhas",
+                    "miles_enterprise" => "Tudo Azul",
+                      "activated_sell" => "1",
+                               "color" => "#00ADEF",
+                              "wsname" => "azul"
+    ),
+          "avianca_miles" => array(
+                    // these fields MUST be edited.
+                               "price" => "300.10",
+                           "available" => "1000000",
+                        "selling_mode" => "1",
+             "maximizing_profit_start" => "0.00",
+        "maximizing_profit_percentage" => "0.00",
+
+                    // leave these as they are.
+                                  "id" => "3",
+                                "hash" => "avianca_miles",
+                             "acronym" => "AVIANCA",
+                             "airline" => "AVIANCA",
+                                "name" => "Milhas Avianca",
+                           "kind_name" => "milhas",
+                    "miles_enterprise" => "Amigos",
+                      "activated_sell" => "0",
+                               "color" => "#EE2229",
+                              "wsname" => "avianca"
+    ),
+     "gol_miles" => array(
+                    // these fields MUST be edited.
+                               "price" => "290.00",
+                           "available" => "1000000",
+                        "selling_mode" => "2",
+             "maximizing_profit_start" => "0.00",
+        "maximizing_profit_percentage" => "0.00",
+
+                    // leave these as they are.
+                                  "id" => "4",
+                                "hash" => "gol_miles",
+                             "acronym" => "SMILES",
+                             "airline" => "GOL",
+                                "name" => "Milhas Gol",
+                           "kind_name" => "milhas",
+                    "miles_enterprise" => "Smiles",
+                      "activated_sell" => "1",
+                               "color" => "#FB8C18",
+                              "wsname" => "gol"
+    )
+);
+
+?>
